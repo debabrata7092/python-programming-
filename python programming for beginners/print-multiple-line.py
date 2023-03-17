1167bcdd574778f6868afe9a6a1acfb5642b5c47
debabrata <debabrata39197@gmail.com>
2023-03-17 20:50:36 +0530

    string pro

--- python programming for beginners/print-multiple-line.py
+++ python programming for beginners/print-multiple-line.py
@@ -1,6 +1,43 @@
+
+
+
+
 a = """Python is a high-level, general-purpose programming language.
  Its design philosophy emphasizes code readability with the use of significant indentation. 
  Python is dynamically typed and garbage-collected. 
  It supports multiple programming paradigms, including structured, 
  object-oriented and functional programming."""
+
 print(a)
+
+
+
+b=("Python is a \n    high-level")
+print (b)
+
+
+
+
+print("hi",6,7, sep="*")
+print
+
+
+
+print(1,2,3,4,5, end="+")
+print(6,7,8,9,10)
+
+
+
+
+
+
+
+coures= "python programing"
+
+print(len(coures))
+print(coures[1])
+print(coures[1:11:4])
+print(coures[0:])
+print(coures.find("n"))
+print(coures.replace("P","t"))
+
